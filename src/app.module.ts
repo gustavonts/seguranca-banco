33f075f678f21947d327e2197fe77f313e923906
Gustavo Guedes <gugaguedes11@gmail.com>
2025-11-07 19:12:44 -0300

    Modulos iniciais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScannerService } from './scanner/scanner.service';
+import { ScannerModule } from './scanner/scanner.module';
+import { DatabaseModule } from './database/database.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [ScannerModule, DatabaseModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ScannerService],
 })
 export class AppModule {}
---
+++ src/scanner/scanner.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('scanner')
+export class ScannerController {}
