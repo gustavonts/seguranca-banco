515e5acc12c550a479002185b0609ee67feffb1d
Gustavo Guedes <gugaguedes11@gmail.com>
2025-11-07 19:17:43 -0300

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ScannerService } from './scanner/scanner.service';
 import { ScannerModule } from './scanner/scanner.module';
 import { DatabaseModule } from './database/database.module';
 import { ReportsModule } from './reports/reports.module';
+import { DatabaseNoSpecService } from './database--no-spec/database--no-spec.service';
 
 @Module({
   imports: [ScannerModule, DatabaseModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService, ScannerService],
+  providers: [AppService, ScannerService, DatabaseNoSpecService],
 })
 export class AppModule {}
